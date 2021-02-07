d81b1cd84cc85c8cca3b2045a7ebc8fd537e162c
TK221 <51761737+TK221@users.noreply.github.com>
2021-02-07 16:42:56 +0100

    New Tetris tiles and drawing them

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,10 +1,23 @@
 #include <SFML/Graphics.hpp>
+using namespace sf;
+
+const unsigned int X = 14;
+const unsigned int Y = 20;
+
+int field[X][Y] = { 0 };
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(320, 480), "Tetris");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    
+    Texture t1;
+    if (!t1.loadFromFile("../Assets/Tiles.png"))
+    {
+        printf("Didnt find tile sprite!");
+    }
+
+    Sprite tiles(t1);
+    tiles.setTextureRect(IntRect(0, 0, 23, 23));
 
     while (window.isOpen())
     {
@@ -16,9 +29,16 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(tiles);
+        tiles.move(0, 10);
+        window.draw(tiles);
         window.display();
     }
 
     return 0;
+}
+
+void drawField()
+{
+
 }
