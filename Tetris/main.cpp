f3a93924d74569778e5e4bd260f8981ea6b8ae36
Edmund Krain <esanbayon@gmail.com>
2021-02-07 18:41:25 +0100

    added funtions: add block to array
    new:
    void printFieldToConsole();
    bool newBlock();
    void clearField();

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,10 +1,59 @@
 #include <SFML/Graphics.hpp>
+#include <Windows.h>
+
 using namespace sf;
 
+//functions
+void printFieldToConsole();
+void drawField();
+bool newBlock();
+void clearField();
+
+//variables
 const unsigned int X = 14;
 const unsigned int Y = 20;
+const unsigned int SquareSize = 23;
+
+int block[7][4][4] =
+{
+    1,0,0,0,
+    1,0,0,0,
+    1,0,0,0,
+    1,0,0,0,
+
+    1,0,0,0,
+    1,1,0,0,
+    0,1,0,0,
+    0,0,0,0,
+
+    0,1,0,0,
+    1,1,0,0,
+    1,0,0,0,
+    0,0,0,0,
+
+    1,1,0,0,
+    1,1,0,0,
+    0,0,0,0,
+    0,0,0,0,
+
+    1,0,0,0,
+    1,1,0,0,
+    1,0,0,0,
+    0,0,0,0,
+
+    1,0,0,0,
+    1,0,0,0,
+    1,1,0,0,
+    0,0,0,0,
+
+    0,1,0,0,
+    0,1,0,0,
+    1,1,0,0,
+    0,0,0,0,
+};
 
 int field[X][Y] = { 0 };
+int currentObjectPosition[4][2] = { 0 };    //[one of the 4 blocks of one "total block"][X and Y]
 
 int main()
 {
@@ -17,7 +66,7 @@ int main()
     }
 
     Sprite tiles(t1);
-    tiles.setTextureRect(IntRect(0, 0, 23, 23));
+    tiles.setTextureRect(IntRect(0, 0, SquareSize, SquareSize));
 
     while (window.isOpen())
     {
@@ -29,12 +78,53 @@ int main()
         }
 
         window.clear();
-        tiles.move(0, 10);
+        Sleep(1000);
+       
+        clearField();
+        if (!newBlock()) {
+            printf("error\n");
+        }
+        printFieldToConsole();
+        tiles.move(0, SquareSize);
         window.draw(tiles);
         window.display();
     }
 
     return 0;
+}
+
+void clearField() {
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            field[i][j] = 0;
+        }
+    }
+}
+
+bool newBlock() {   //returns false if collision
+    int type = rand() % 7;
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (block[type][i][j] == 1) {
+                if (field[i][j + (X / 2)] == 0) {
+                    field[i][j + (X / 2)] = 1;
+                }
+                else return false;
+            }
+        }
+    }
+    return true;
+}
+
+void printFieldToConsole() {
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < Y; j++) {
+            printf("%i", field[i][j]);
+        }
+        printf("\n");
+    }
+    printf("============\n");
 }
 
 void drawField()
